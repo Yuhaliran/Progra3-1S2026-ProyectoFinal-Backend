b2f1ee1c3e271b91cac257756ec36cb68094d399
Anelyn Laines <anelynlaines@outlook.com>
2026-05-17 09:17:12 -0600

    Agregar linea de prueba

--- ProyectoFinal.Progra3.Backend/ProyectoFinal.Progra3.Backend/WeatherForecast.cs
+++ ProyectoFinal.Progra3.Backend/ProyectoFinal.Progra3.Backend/WeatherForecast.cs
@@ -6,7 +6,9 @@ namespace ProyectoFinal.Progra3.Backend
 
         public int TemperatureC { get; set; }
 
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
 
         public string? Summary { get; set; }
     }
