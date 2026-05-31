864b297f5804959867b0cda6bd9dc8295418b1a2
nuryan-admin <misa7w7@gmail.com>
2026-05-30 18:00:30 -0600

    AjustarScritp006

--- ProyectoFinal.Progra3.Backend/ProyectoFinal.Progra3.Backend/Database/006_AgregarFechaCreacionColaLectura.sql
+++ ProyectoFinal.Progra3.Backend/ProyectoFinal.Progra3.Backend/Database/006_AgregarFechaCreacionColaLectura.sql
@@ -1,2 +1,10 @@
-ALTER TABLE dbo.ColaLectura 
-ADD FechaCreacion DATETIME NOT NULL DEFAULT GETDATE();
+IF NOT EXISTS (
+    SELECT 1 
+    FROM sys.columns 
+    WHERE object_id = OBJECT_ID('dbo.ColaLectura') 
+      AND name = 'FechaCreacion'
+)
+BEGIN
+    ALTER TABLE dbo.ColaLectura 
+    ADD FechaCreacion DATETIME NOT NULL DEFAULT GETDATE();
+END
